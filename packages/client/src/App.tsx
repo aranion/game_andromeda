b2ebcb7b9e9c42dde4d4be0cc4d785392fcea648
GavrilejES <Gavrilej.ES@nppgamms.ru>
2022-10-26 14:03:02 +0300

    Fix. Test error server.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,6 @@
 import './App.css';
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Router } from './router';
-import { RouterList } from './router/routerList';
 
 function App() {
   useEffect(() => {
@@ -18,16 +16,6 @@ function App() {
 
   return (
     <main>
-      {/* Links для проверки роутера, при добавлении страниц можно удалить */}
-      <Link to={RouterList.HOME}>HOME</Link> -
-      <Link to={RouterList.SIGN_IN}>SIGN_IN</Link> -
-      <Link to={RouterList.SIGN_UP}>SIGN_UP</Link> -
-      <Link to={RouterList.SERVER_ERROR}>SERVER_ERROR</Link> -
-      <Link to={RouterList.NOT_FOUND}>NOT_FOUND</Link> -
-      <Link to={RouterList.GAME}>GAME</Link> -
-      <Link to={RouterList.FORUM}>FORUM</Link> -
-      <Link to={RouterList.LEADER_BOARD}>LEADER_BOARD</Link> -
-      <Link to={RouterList.PROFILE}>PROFILE</Link> -
       <section>
         <Router />
       </section>
