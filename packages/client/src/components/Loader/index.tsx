57f93a6023aef23789ab86ff87e8e5e67ea902c0
gavrileyes <gavriley-es@mail.ru>
2022-10-31 03:07:12 +0300

    Компонент с аватаркой

--- packages/client/src/components/Loader/index.tsx
+++ packages/client/src/components/Loader/index.tsx
@@ -1,3 +1,11 @@
+import './style.css';
+
 export function Loader() {
-  return <div>Loader...</div>;
+  return (
+    <div className='loader'>
+      <div className='loader__inner loader__inner_one'></div>
+      <div className='loader__inner loader__inner_two'></div>
+      <div className='loader__inner loader__inner_three'></div>
+    </div>
+  );
 }
