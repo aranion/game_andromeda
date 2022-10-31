465d836a5e1feb44c8f8fcbaa10962ef3e31ea5d
GavrilejES <Gavrilej.ES@nppgamms.ru>
2022-10-31 14:57:14 +0300

    add plagins postCSS

--- packages/client/postcss.config.cjs
+++ packages/client/postcss.config.cjs
@@ -3,6 +3,8 @@ module.exports = ctx => ({
   plugins: {
     'postcss-nested': ctx.options?.plugin && {},
     'postcss-import': ctx.options?.plugin && {},
+    'postcss-easings': ctx.options?.plugin && {},
     autoprefixer: ctx.options?.plugin && {},
+    cssnext: ctx.options?.plugin && {},
   },
 });
