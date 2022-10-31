bb8f3c8826274cfedf7c1f8f315b6537a4a5300f
Iriswewq <75016726+elaineir@users.noreply.github.com>
2022-10-31 16:57:01 +0400

    feature/AND-72 Создание базового движка игры с игровым циклом
    * feature/AND-72 Создание базового движка игры с игровым циклом и объектом игрока
    
    * feature/AND-72 Фикс конфликтов с jest
    
    * feature/AND-72 Исправлены ошибки, выявленные на ревью
    
    * feature/AND-72 canvas и ctx возвращены в классы GameObject, GameMap и DirectionsInput в кач-ве свойств

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,14 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Game from './pages/Game';
-
-const appContent = 'Вот тут будет жить ваше приложение :)';
 
 // @ts-ignore
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
 );
 
-test('Example test', async () => {
-  render(<Game />);
-  expect(screen.getByText(appContent)).toBeDefined();
+test('Canvas initialization test', async () => {
+  const { container } = render(<Game />);
+  const canvas = container.querySelector('canvas');
+  expect(canvas).toBeDefined();
 });
---
+++ packages/client/src/pages/Game/core/overworld/directions-input/types.ts
@@ -0,0 +1,3 @@
+export type DirectionsInputConfig = {
+  canvas: HTMLCanvasElement;
+};
