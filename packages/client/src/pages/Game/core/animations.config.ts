e392a807722b8ef33d7f791a8c6e961464d2703d
Iriswewq <75016726+elaineir@users.noreply.github.com>
2022-11-02 12:02:32 +0400

    feature/AND-77 Реализовать класс Sprite
    * feature/AND-77 Реализация класса Sprite с циклом и конфигом анимаций
    
    * feature/AND-77 Добавление метода updateSkin в updateLives в классе Player
    
    * feature/AND-77 Декларация модуля png
    
    * feature/AND-77 Фикс мока png для jest
    
    * feature/AND-77 Увеличен размер спрайта
    
    * feature/AND-77 Возвращено css-свойство image-rendering: pixelated

---
+++ packages/client/src/pages/Game/core/animations.config.ts
@@ -0,0 +1,12 @@
+import { playerAnimations } from './entities/player/animations';
+import { AnimationKey } from './constants';
+
+export type AnimationType = { [key in AnimationKey]: [number, number][] };
+
+/**
+ * Конфигурация всех анимаций игры: объект из анимаций по ключу и двумерного массива --
+ * разбивкой по кадрам `fly: [[0,0], [1,0], [2,0], [3,0]]`.
+ * */
+export const animationsConfig: AnimationType = {
+  ...playerAnimations,
+};
