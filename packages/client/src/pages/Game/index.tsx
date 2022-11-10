5f382c70109aaeb00d6af7b64466a44da656053b
gavrileyes <gavriley-es@mail.ru>
2022-11-10 13:45:32 +0300

    Add cursor game

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,3 +1,4 @@
+import srcCursor from 'src/assets/imgs/curcorGame.png';
 import { useLayoutEffect, useRef } from 'react';
 import { Game } from './core';
 import './game.css';
@@ -5,15 +6,33 @@ import './game.css';
 export default function GamePage() {
   const canvas = useRef<HTMLCanvasElement>(null);
   const game = useRef<Game | null>(null);
+  const cursor = useRef<HTMLImageElement | null>(null);
+
+  const moveCursor = (e: MouseEvent) => {
+    if (cursor?.current) {
+      const { pageY, pageX } = e;
+
+      cursor.current.style.transform = `translate(${pageX}px, ${pageY}px)`;
+    }
+  };
 
   useLayoutEffect(() => {
     if (canvas?.current) {
       game.current = new Game({ canvas: canvas.current });
       game.current?.init();
-    }
 
-    return () => game?.current?.unmount();
+      canvas.current.addEventListener('mousemove', moveCursor);
+    }
+    return () => {
+      game?.current?.unmount();
+      canvas.current?.removeEventListener('mousemove', moveCursor);
+    };
   }, [canvas]);
 
-  return <canvas className={'canvas'} ref={canvas}></canvas>;
+  return (
+    <>
+      <canvas className='canvas' ref={canvas}></canvas>
+      <img className='cursor' ref={cursor} src={srcCursor} />
+    </>
+  );
 }
