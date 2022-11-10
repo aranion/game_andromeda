5413eb29826f76d05af7e726b4aa12292efc55a5
gavrileyes <gavriley-es@mail.ru>
2022-11-10 14:26:49 +0300

    убрал курсор

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,4 +1,3 @@
-import srcCursor from 'src/assets/imgs/curcorGame.png';
 import { useLayoutEffect, useRef } from 'react';
 import { Game } from './core';
 import './game.css';
@@ -6,33 +5,14 @@ import './game.css';
 export default function GamePage() {
   const canvas = useRef<HTMLCanvasElement>(null);
   const game = useRef<Game | null>(null);
-  const cursor = useRef<HTMLImageElement | null>(null);
-
-  const moveCursor = (e: MouseEvent) => {
-    if (cursor?.current) {
-      const { pageY, pageX } = e;
-
-      cursor.current.style.transform = `translate(${pageX}px, ${pageY}px)`;
-    }
-  };
 
   useLayoutEffect(() => {
     if (canvas?.current) {
       game.current = new Game({ canvas: canvas.current });
       game.current?.init();
-
-      canvas.current.addEventListener('mousemove', moveCursor);
     }
-    return () => {
-      game?.current?.unmount();
-      canvas.current?.removeEventListener('mousemove', moveCursor);
-    };
+    return () => game?.current?.unmount();
   }, [canvas]);
 
-  return (
-    <>
-      <canvas className='canvas' ref={canvas}></canvas>
-      <img className='cursor' ref={cursor} src={srcCursor} />
-    </>
-  );
+  return <canvas className='canvas' ref={canvas}></canvas>;
 }
