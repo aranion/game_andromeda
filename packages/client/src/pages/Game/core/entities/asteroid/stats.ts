e54972221dc3440cd7a0f9d88ee5af8f69e5fa03
Alyona Irisova <elaineirr.910@gmail.com>
2022-11-14 19:44:53 +0400

    feature/AND-85 Изменен спрайт и размер астероидов

--- packages/client/src/pages/Game/core/entities/asteroid/stats.ts
+++ packages/client/src/pages/Game/core/entities/asteroid/stats.ts
@@ -1,14 +1,14 @@
 import { AsteroidConfig } from './types';
-import asteroid1 from './../../assets/asteriods/asteroid1.png';
-import asteroid2 from './../../assets/asteriods/asteroid2.png';
-import asteroid3 from './../../assets/asteriods/asteroid3.png';
-import asteroid4 from './../../assets/asteriods/asteroid4.png';
+import asteroid1 from '../../assets/asteroid/asteroid1.png';
+import asteroid2 from '../../assets/asteroid/asteroid2.png';
+import asteroid3 from '../../assets/asteroid/asteroid3.png';
+import asteroid4 from '../../assets/asteroid/asteroid4.png';
 import { randomInteger } from '../../utils/random-integer';
 
 const defaultAsteroidStats = {
   maxSpeed: 10,
   maxRotateSpeed: 0.1,
-  radius: 20,
+  radius: 40,
   isAnimated: false,
 };
 
