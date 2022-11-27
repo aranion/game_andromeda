04ded8cd3be99c7cc568f8a531fc1d2a89e72fcc
Evgenii <71134155+aranion@users.noreply.github.com>
2022-11-27 17:46:30 +0300

    feature/AND-GeolocationError
    Co-authored-by: GavrilejES <Gavrilej.ES@nppgamms.ru>

--- packages/client/src/hooks/useGeolocation.ts
+++ packages/client/src/hooks/useGeolocation.ts
@@ -29,6 +29,21 @@ export const useGeolocation = () => {
   };
 
   const onError = (error: GeolocationPositionError) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        console.log('User denied the request for Geolocation.');
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log('Location information is unavailable.');
+        break;
+      case error.TIMEOUT:
+        console.log('The request to get user location timed out.');
+        break;
+      default:
+        console.log('Geolocation: An unknown error occurred.');
+        break;
+    }
+
     setLocation({
       loaded: true,
       error,
