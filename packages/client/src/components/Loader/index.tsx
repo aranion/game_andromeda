078004028d9fe6aaefd3d883c66791f8df02d0c2
Evgenii <71134155+aranion@users.noreply.github.com>
2022-11-28 19:48:33 +0300

    feature/AND-35 Redux/api Profile (#30)
    * feature/AND-35
    
    * fix
    
    * отключение истории браузера при редактировании профиля
    
    * fix profile
    
    * fix
    
    * replace profile edit

--- packages/client/src/components/Loader/index.tsx
+++ packages/client/src/components/Loader/index.tsx
@@ -1,16 +1,25 @@
 import classNames from 'classnames';
 import cls from './styles.module.css';
 
-export function Loader() {
+export function Loader(props: Props) {
+  const { position = 'absolute' } = props;
+
+  const clsLoader = classNames(cls.loader, {
+    [cls.loader__relative]: position === 'relative',
+  });
   const clsOne = classNames(cls.loader__inner, cls.loader__inner_one);
   const clsTwo = classNames(cls.loader__inner, cls.loader__inner_two);
   const clsThree = classNames(cls.loader__inner, cls.loader__inner_three);
 
   return (
-    <div className={cls.loader}>
+    <div className={clsLoader}>
       <div className={clsOne}></div>
       <div className={clsTwo}></div>
       <div className={clsThree}></div>
     </div>
   );
 }
+
+type Props = {
+  position?: 'absolute' | 'relative';
+};
