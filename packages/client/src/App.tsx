46a1c2c1c3b3171cdc8535d73ee1d3b3738812bd
gavrileyes <gavriley-es@mail.ru>
2022-12-02 23:30:21 +0300

    test

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,56 +8,56 @@ import { RouterList } from './router/routerList';
 import { ButtonFullscreen } from './components';
 
 function App() {
-  // const { checkIsAuth } = useAuth();
+  const { checkIsAuth } = useAuth();
 
-  // const { pathname } = useLocation();
-  // const refWrapper = useRef(null);
+  const { pathname } = useLocation();
+  const refWrapper = useRef(null);
   const fullscrinableElem = useRef(null);
 
-  // const startStarts = useCallback(() => {
-  //   const isNotGamePage = pathname.toLocaleLowerCase() !== RouterList.GAME;
+  const startStarts = useCallback(() => {
+    const isNotGamePage = pathname.toLocaleLowerCase() !== RouterList.GAME;
 
-  //   if (refWrapper && isNotGamePage) {
-  //     const { amount, size, duration } = CONFIG_STARS_PARAMS;
-  //     const { giant, max, min } = size;
+    if (refWrapper && isNotGamePage) {
+      const { amount, size, duration } = CONFIG_STARS_PARAMS;
+      const { giant, max, min } = size;
 
-  //     const randomBetween = (a: number, b: number) => {
-  //       return a + Math.random() * (b - a);
-  //     };
+      const randomBetween = (a: number, b: number) => {
+        return a + Math.random() * (b - a);
+      };
 
-  //     const fragment = document.createDocumentFragment();
+      const fragment = document.createDocumentFragment();
 
-  //     for (let i = 0; i < amount; i++) {
-  //       const starElem = document.createElement('div');
-  //       const sizeElem =
-  //         Math.round(Math.random() * 10) === 0
-  //           ? giant
-  //           : randomBetween(min, max);
+      for (let i = 0; i < amount; i++) {
+        const starElem = document.createElement('div');
+        const sizeElem =
+          Math.round(Math.random() * 10) === 0
+            ? giant
+            : randomBetween(min, max);
 
-  //       starElem.classList.add(cls.app__star);
+        starElem.classList.add(cls.app__star);
 
-  //       starElem.style.width = `${sizeElem}px`;
-  //       starElem.style.height = `${sizeElem}px`;
-  //       starElem.style.left = `${randomBetween(0, 100)}%`;
-  //       starElem.style.top = `${randomBetween(0, 100)}%`;
-  //       starElem.style.boxShadow = `0 0 ${sizeElem}px ${
-  //         sizeElem / 2
-  //       }px #043668`;
-  //       starElem.style.animationDuration = `${randomBetween(
-  //         duration.min,
-  //         duration.max
-  //       )}s`;
+        starElem.style.width = `${sizeElem}px`;
+        starElem.style.height = `${sizeElem}px`;
+        starElem.style.left = `${randomBetween(0, 100)}%`;
+        starElem.style.top = `${randomBetween(0, 100)}%`;
+        starElem.style.boxShadow = `0 0 ${sizeElem}px ${
+          sizeElem / 2
+        }px #043668`;
+        starElem.style.animationDuration = `${randomBetween(
+          duration.min,
+          duration.max
+        )}s`;
 
-  //       fragment.append(starElem);
-  //     }
+        fragment.append(starElem);
+      }
 
-  //     (refWrapper.current as HTMLDivElement | null)?.after(fragment);
-  //   }
-  // }, [refWrapper]);
+      (refWrapper.current as HTMLDivElement | null)?.after(fragment);
+    }
+  }, [refWrapper]);
 
   useEffect(() => {
-    // checkIsAuth();
-    // startStarts();
+    checkIsAuth();
+    startStarts();
 
     const fetchServerData = async () => {
       const url = `http://localhost:${__SERVER_PORT__}`;
@@ -70,13 +70,11 @@ function App() {
 
   return (
     <div className={cls.app} ref={fullscrinableElem}>
-      REEEEEAAACT
-      {/* <main className={cls.app__content}>
+      <main className={cls.app__content}>
         <Router />
       </main>
       <ButtonFullscreen elemRef={fullscrinableElem} />
-
-      <div ref={refWrapper}></div> */}
+      <div ref={refWrapper}></div>
     </div>
   );
 }
