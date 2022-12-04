43a09b309dd6f0abd7e7be297beb24c74759ec86
gavrileyes <gavriley-es@mail.ru>
2022-12-04 20:44:43 +0300

    fix folder

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,7 +7,7 @@ import { createClientAndConnect } from './db';
 import path from 'path';
 import fs from 'fs';
 // @ts-ignore
-import { render } from '../dist/ssr/entry-server.cjs';
+import { render } from '../../client/dist/ssr/entry-server.cjs';
 
 const app = express();
 app.use(cors());
@@ -17,13 +17,16 @@ createClientAndConnect();
 
 app.get('/', (_: Request, res: Response) => {
   const result = render();
-  const template = path.resolve(__dirname, '../dist/client/index.html');
+  const template = path.resolve(
+    __dirname,
+    '../../client/dist/client/index.html'
+  );
   const htmlString = fs.readFileSync(template, 'utf-8');
   const newString = htmlString.replace('<!--ssr-outlet-->', result);
   res.send(newString);
 });
 
-app.use(express.static(path.resolve(__dirname, '../dist/client')));
+app.use(express.static(path.resolve(__dirname, '../../client/dist/client')));
 
 app.listen(port, () => {
   console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
