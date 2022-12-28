6082cc3ab94dfb81460b9d302354fae88ccacefd
Eka Hvostova <ekasagalaeva@gmail.com>
2022-12-28 19:44:58 +0300

    Feature/AND-100 (#46)
    * add dark theme
    
    * fix input style
    
    * fix review попытка 1
    
    * fix type
    
    * Changes light theme
    
    * small tweak to --color-grey in light theme
    
    Co-authored-by: gavrileyes <gavriley-es@mail.ru>
    Co-authored-by: herehavetheseflowers <herehavetheseflowers@gmail.com>

---
+++ packages/client/src/hooks/useDarkTheme.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+const enum ThemeList {
+  Light = 'light',
+  Dark = 'dark',
+}
+
+export const useDarkTheme = (): [string, boolean, () => void] => {
+  const [theme, setTheme] = useState<ThemeList>(ThemeList.Light);
+  const varTheme = 'theme';
+
+  const setMode = (mode: ThemeList) => {
+    window.localStorage.setItem(varTheme, mode);
+    setTheme(mode);
+  };
+
+  const isThemeMode = theme === ThemeList.Dark;
+
+  const toggleTheme = () => {
+    theme === ThemeList.Light
+      ? setMode(ThemeList.Dark)
+      : setMode(ThemeList.Light);
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem(
+      varTheme
+    ) as ThemeList | null;
+
+    window.matchMedia &&
+    window.matchMedia(`(prefers-color-scheme: ${ThemeList.Dark})`).matches &&
+    !localTheme
+      ? setTheme(ThemeList.Dark)
+      : localTheme
+      ? setTheme(localTheme)
+      : setMode(ThemeList.Light);
+  }, []);
+
+  return [theme, isThemeMode, toggleTheme];
+};
