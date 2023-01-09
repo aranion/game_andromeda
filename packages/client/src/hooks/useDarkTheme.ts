9341ae45868ce5760bbeefdb3b1352881063fcf8
gavrileyes <gavriley-es@mail.ru>
2023-01-10 01:09:14 +0300

    Изменение темы фона в игре

--- packages/client/src/hooks/useDarkTheme.ts
+++ packages/client/src/hooks/useDarkTheme.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const enum ThemeList {
+export const enum ThemeList {
   Light = 'light',
   Dark = 'dark',
 }
--- packages/client/src/pages/Game/core/overworld/game-map/types.ts
+++ packages/client/src/pages/Game/core/overworld/game-map/types.ts
@@ -2,10 +2,12 @@ import type { Player } from '../../entities/player';
 import type { GameObject } from '../../entities/game-object';
 import type { CanvasProperties, GameMapConfig } from '../../types';
 import type { SceneTransition } from '../scene-transition';
+import type { GameTheme } from '../game-theme';
 
 export type GameMapConstrConfig = GameMapConfig & {
   player: Player;
   sceneTransition: SceneTransition;
+  gameTheme: GameTheme;
 } & CanvasProperties;
 
 export type UpdateParams = {
