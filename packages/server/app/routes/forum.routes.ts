2b4435a8c81e8827b74bec86c78c1e326db91826
gavrileyes <gavriley-es@mail.ru>
2023-01-21 16:57:08 +0300

    fix router api

--- packages/server/app/routes/forum.routes.ts
+++ packages/server/app/routes/forum.routes.ts
@@ -5,42 +5,36 @@ import * as commentController from '../controllers/comment.controller';
 const router = express.Router();
 
 // Получение всех тем форума
-router.get('/', topicController.getAllTopics);
-
+router.get('/topics', topicController.getAllTopics);
 // Удаление всех тем форума
-router.delete('/', topicController.deleteAllTopics);
-
+router.delete('/topics', topicController.deleteAllTopics);
 // Создание темы форума
-router.post('/', topicController.createTopic);
+router.post('/topics', topicController.createTopic);
 
 // Обновление темы форума по ее ID
-router.put('/:topicId', topicController.updateTopicById);
-
+router.put('/topics/:topicId', topicController.updateTopicById);
 // Получение темы форума по ID
-router.get('/:topicId', topicController.getTopicById);
-
+router.get('/topics/:topicId', topicController.getTopicById);
 // Удаление темы форума по ID
-router.delete('/:topicId', topicController.deleteTopicById);
+router.delete('/topics/:topicId', topicController.deleteTopicById);
 
 // Создание комментария
-router.post('/:topicId', commentController.createComment);
-
+router.post('/comment/:topicId', commentController.createComment);
 // Обновление комментария по ID
-router.put('/:topicId', commentController.updateCommentById);
-
+router.put('/comment/:topicId', commentController.updateCommentById);
 // Получение комментария по ID
-router.get('/:topicId', commentController.getCommentById);
-
+router.get('/comment/:topicId', commentController.getCommentById);
 // Удаление комментария по ID
-router.delete('/:topicId', commentController.deleteCommentById);
+router.delete('/comment/:topicId', commentController.deleteCommentById);
 
 // Получение всех комментариев темы по ее Id
-router.get('/:topicId', commentController.getAllComments);
-
+router.get('/comments/:topicId', commentController.getAllComments);
 // Получение колличества комментариев темы по ee ID
-router.get('/:topicId', commentController.getAllTopicCommentsCount);
-
+router.get('/comments/:topicId', commentController.getAllTopicCommentsCount);
 // Удаление всех комментариев темы по ee ID
-router.delete('/:topicId', commentController.deleteAllCommentsByTopicId);
+router.delete(
+  '/comments/:topicId',
+  commentController.deleteAllCommentsByTopicId
+);
 
 export default router;
